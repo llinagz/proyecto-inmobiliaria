d9834923bd4145c2872315022df0bd57e4f42619
Javi <javierllinaresbarral@outlook.com>
2023-09-04 09:30:00 +0200

    Version inicial del CRUD lista, bocetado

--- anuncio.php
+++ anuncio.php
@@ -1,22 +1,42 @@
 <?php
-  require 'includes/funciones.php';
 
+  //Validar la URL por ID valido
+  $id = $_GET['id'];
+  $id = filter_var($id, FILTER_VALIDATE_INT);
+
+  if(!$id){
+    header('Location: /');
+  }
+
+  //Importar la conexion
+  require 'includes/config/database.php';
+  $db = conectarDB();
+  //Consultar
+  $query = "SELECT * FROM propiedades WHERE id = {$id}";
+  //Leer resultado
+  $resultado = mysqli_query($db, $query);
+
+  //Accedemos a las propiedades de un objeto con -> y sin comillas
+  //Aqui lo que hacemos es que si el resultado de las columnas es inexistente vamos a la principal
+  if($resultado->num_rows === 0){
+    header('Location: /');
+  }
+
+  $propiedad = mysqli_fetch_assoc($resultado);
+
+  require 'includes/funciones.php';
   incluirTemplate('header');
 ?>
 
     <main class="contenedor seccion contenido-centrado">
-      <h1>Casa en venta frente al bosque</h1>
-      <picture>
-        <source srcset="build/img/destacada.webp" type="image/webp" />
-        <source srcset="build/img/destacada.jpg" type="image/jpg" />
-        <img
-          loading="lazy"
-          src="build/img/destacada.jpg"
-          alt="imagen de la propiedad"
-        />
-      </picture>
+      <h1><?php echo $propiedad['titulo']; ?></h1>
+      <img
+        loading="lazy"
+        src="/imagenes/<?php echo $propiedad['imagen']; ?>"
+        alt="imagen de la propiedad"
+      />
       <div class="resumen-propiedad">
-        <p class="precio">1.500.000€</p>
+        <p class="precio"><?php echo $propiedad['precio']; ?>€</p>
         <ul class="iconos-caracteristicas">
           <li>
             <img
@@ -25,7 +45,7 @@
               src="build/img/icono_wc.svg"
               alt="icono wc"
             />
-            <p>3</p>
+            <p><?php echo $propiedad['wc']; ?></p>
           </li>
           <li>
             <img
@@ -34,7 +54,7 @@
               src="build/img/icono_estacionamiento.svg"
               alt="icono wc"
             />
-            <p>3</p>
+            <p><?php echo $propiedad['estacionamiento']; ?></p>
           </li>
           <li>
             <img
@@ -43,18 +63,16 @@
               src="build/img/icono_dormitorio.svg"
               alt="icono wc"
             />
-            <p>4</p>
+            <p><?php echo $propiedad['habitaciones']; ?></p>
           </li>
         </ul>
         <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione,
-          eius expedita quos, accusantium odio consequuntur qui eaque
-          repellendus mollitia inventore impedit blanditiis. Doloremque placeat
-          blanditiis minus voluptas ullam nulla maxime.
+          <?php echo $propiedad['descripcion']; ?>
         </p>
       </div>
     </main>
 
 <?php
+  mysqli_close($db);
   incluirTemplate('footer');
 ?>
